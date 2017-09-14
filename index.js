492798ac7a1b7b83dcad4ec2eced130179e35a61
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 19:20:07 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,21 +22,21 @@ function destructivelyRemoveFirstKitten () {
 }
 
 function appendKitten (name) {
-  newKittens = [...kittens, name];
+  var newKittens = [...kittens, name];
   return newKittens;
 }
 
 function prependKitten (name) {
-  newKittens = [name, ...kittens];
+  var newKittens = [name, ...kittens];
   return newKittens;
 }
 
 function removeLastKitten () {
-  newKittens = kittens.slice(0, kittens.length - 1);
+  var newKittens = kittens.slice(0, kittens.length - 1);
   return newKittens;
 }
 
 function removeFirstKitten () {
-  newKittens = kittens.slice(1);
+  var newKittens = kittens.slice(1);
   return newKittens;
 }

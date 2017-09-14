9f8d9c9580c2a15b869f75e7631bd35856788202
Harold Yang <harold.h.yang@gmail.com>
2017-09-14 19:12:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
 const app = "I don't do much."
+
+var kittens = ["Milo", "Otis", "Garfield"];
+
+function destructivelyAppendKitten (name) {
+  kittens.push(name);
+  return kittens;
+}
+
+function destructivelyPrependKitten (name) {
+  kittens.unshift(name);
+  return kittens;
+}
+
+function destructivelyRemoveLastKitten () {
+  kittens.pop();
+  return kittens;
+}
+
+function destructivelyRemoveFirstKitten () {
+  kittens.shift();
+  return kittens;
+}
+
+function appendKitten (name) {
+  kittens = [...kittens, name];
+  return kittens;
+}
+
+function prependKitten (name) {
+  kittens = [name, ...kittens];
+  return kittens;
+}
+
+function removeLastKitten () {
+  kittens = kittens.slice(0, kittens.length - 1);
+  return kittens;
+}
+
+function removeFirstKitten () {
+  kittens = kittens.slice(1);
+  return kittens;
+}
